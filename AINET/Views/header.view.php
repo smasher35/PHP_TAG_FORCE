acfdd07121b564465c7c1fd9c6f0d76cd4d94274
Rmiguel <ruben.miguel.55@gmail.com>
2015-04-20 11:57:40 +0100

    Resolvido "Back To Top" link
    Resolvido "Back To Top" link

--- AINET/Views/header.view.php
+++ AINET/Views/header.view.php
@@ -1,62 +1,63 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1"><!-- por causa das resoluções dos ecrãs móveis -->
-	<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-	<title>PHP TAG FORCE</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1"><!-- por causa das resoluções dos ecrãs móveis -->
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>PHP TAG FORCE</title>
 
-	<!-- Personal Styles -->
-	<link rel="stylesheet" type="text/css" href="Styles/styles.css">
+    <!-- Personal Styles -->
+    <link rel="stylesheet" type="text/css" href="Styles/styles.css">
 
-	<!-- Bootstrap -->
-	<link href="Bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Bootstrap -->
+    <link href="Bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
-	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-	<!--[if lt IE 9]>
-	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-	<![endif]-->
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
 </head>
 
 <body>
 
 <header>
-	<!-- NAVIGATION BAR -->
-	<div class="navbar-fixed-top navbar-inverse" id="navegacao">
-		<div class="container">
-			<div class="row"></div>
-				<div class="navbar-brand">
-					<a href="#">PHP TAG FORCE</a>
-				</div>
+    <!-- NAVIGATION BAR -->
+    <a id="home">Useful Tips Section</a>
+    <div class="navbar-fixed-top navbar-inverse" id="navegacao">
+        <div class="container">
+            <div class="row"></div>
+                <div class="navbar-brand">
+                    <a href="#">PHP TAG FORCE</a>
+                </div>
 
-				<div class="navbar-form">
-					<div class="container">
-						<form class="form-search " length="150">
-							<input type="text" class="span3 search-query" placeholder="Search...">
-							<button class="btn btn-primary">Search</button>
+                <div class="navbar-form">
+                    <div class="container">
+                        <form class="form-search " length="150">
+                            <input type="text" class="span3 search-query" placeholder="Search...">
+                            <button class="btn btn-primary">Search</button>
 
-						</form>
-					</div>
-				</div>
+                        </form>
+                    </div>
+                </div>
 
-				<button class="navbar-toggle " data-toggle="collapse" data-target =".navHeaderCollapse">
-					Menu
-				</button>
-				<div class="collapse navbar-collapse navHeaderCollapse">
-					<ul class="nav navbar-nav navbar-right">
-						<li><a href="#home">Home</a></li>
-						<li><a href="#about">About</a></li>
-						<li><a href="#projects">Projects</a></li>
-						<li><a href="#">Contact</a></li>
-						<li><a href="#">Sign Up</a></li>
-						<li><a href="#">Login</a></li>
-					</ul>
-				</div>
-			</div>
-		</div>
-	</div>
+                <button class="navbar-toggle " data-toggle="collapse" data-target =".navHeaderCollapse">
+                    Menu
+                </button>
+                <div class="collapse navbar-collapse navHeaderCollapse">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="#home">Home</a></li>
+                        <li><a href="#about">About</a></li>
+                        <li><a href="#projects">Projects</a></li>
+                        <li><a href="#">Contact</a></li>
+                        <li><a href="#">Sign Up</a></li>
+                        <li><a href="#">Login</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
 </header>
 <div id="home"></div>
