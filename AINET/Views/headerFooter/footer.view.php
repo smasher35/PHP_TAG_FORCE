97b3d0af85736bb6a1771a8215a819a3d26f93eb
Paulo Penicheiro <smasher35@gmail.com>
2015-04-24 11:14:40 +0100

    social network Icons no footer (TESTE)

--- AINET/Views/headerFooter/footer.view.php
+++ AINET/Views/headerFooter/footer.view.php
@@ -1,59 +1,73 @@
-		<div class="alt2">
-			<div class="container">
-				<footer>
-					<button type="button" class="btn btn-link" data-toggle="modal" data-target="#modal-1">&copy; PHP TAG FORCE - 2015</button>
-					<br/> <a href="#home" class="go-top"><h3 class="glyphicon glyphicon-chevron-up"></h3></a>
 
+<div class="alt2">
+		<footer>
+			<div class="col-md-3">
+				Contacts
+			</div>
 
-					<div class="modal fade" id="modal-1">
-						<div class="modal-dialog">
-							<div class="modal-content">
-								<div class="modal-header">
-									<h3 class="modal-title">The Crew</h3>
-								</div>
-								<div class="modal-body">
-										<div class="panel panel-primary">
-											<div class="panel-heading"<h3 class="panel-title">
-												The Programmers</h3>
-										</div>
+			<div class="col-md-3">
+				Follow us on Social Networks
+				<div class="container" id="socialNetworksIcons">
+					<a href="#"><i class="fa fa-facebook-square fa-2x"></i></a>
+					<a href="#"><i class="fa fa-twitter-square fa-2x"></i></a>
+					<a href="#"><i class="fa fa-google-plus-square fa-2x"></i> </a>
+				</div>
 
+			</div>
 
-										<table class="table">
-											<tr>
-												<td><img src="Images/crew/paulo.jpg" class="img-circle img-responsive" id="imgTable"></td>
-												<td>Paulo Penicheiro<br>2130628</td>
-											</tr>
+			<div class="col-md-3">
+				Newsletter
+			</div>
 
-											<tr>
-												<td><img src="Images/crew/ruben.jpg" class="img-circle img-responsive" id="imgTable"></td>
-												<td>Ruben Miguel</td>
-											</tr>
+			<div class="col-md-3">
+				<button type="button" class="btn btn-link" data-toggle="modal" data-target="#modal-1">&copy; PHP TAG FORCE - 2015</button>
+				<br/> <a href="#home" class="go-top"><h3 class="glyphicon glyphicon-chevron-up"></h3></a>
+			</div>
 
-											<tr>
-												<td><img src="Images/crew/alfredo.jpg" class="img-circle img-responsive" id="imgTable"></td>
-												<td>Alfredo Rosa</td>
-											</tr>
-
-
-										</table>
-
-										<div class="panel-footer">
-											&copy; PHP TAG FORCE - All rights reserved
-										</div>
+			<div class="modal fade" id="modal-1">
+				<div class="modal-dialog">
+					<div class="modal-content">
+						<div class="modal-header">
+							<h3 class="modal-title">The Crew</h3>
+						</div>
+						<div class="modal-body">
+							<div class="panel panel-primary">
+								<div class="panel-heading"<h3 class="panel-title">
+									The Programmers</h3>
 								</div>
 
+							<table class="table">
+								<tr>
+									<td><img src="Images/crew/paulo.jpg" class="img-circle img-responsive" id="imgTable"></td>
+									<td>Paulo Penicheiro<br>2130628</td>
+								</tr>
 
-								</div>
-								<div class="modal-footer">
-									<a href="" class="btn btn-primary" data-dismiss="modal">Close</a>
-								</div>
+								<tr>
+									<td><img src="Images/crew/ruben.jpg" class="img-circle img-responsive" id="imgTable"></td>
+									<td>Ruben Miguel</td>
+								</tr>
+
+								<tr>
+									<td><img src="Images/crew/alfredo.jpg" class="img-circle img-responsive" id="imgTable"></td>
+									<td>Alfredo Rosa</td>
+								</tr>
+
+							</table>
+
+							<div class="panel-footer">
+								&copy; PHP TAG FORCE - All rights reserved
 							</div>
 						</div>
 					</div>
-				</footer>
-			</div>
 
-		</div>
+					<div class="modal-footer">
+						<a href="" class="btn btn-primary" data-dismiss="modal">Close</a>
+					</div>
+				</div>
+			</div>
+		</footer>
+</div>
+
 
 
 		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
