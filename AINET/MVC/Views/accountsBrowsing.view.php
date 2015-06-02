8c047e1788f6f96a0b3df666397a521ec1c9d2f0
Paulo Penicheiro <smasher35@gmail.com>
2015-06-02 21:59:05 +0000

    minor changes
    arrumação de código

--- AINET/MVC/Views/accountsBrowsing.view.php
+++ AINET/MVC/Views/accountsBrowsing.view.php
@@ -76,9 +76,7 @@
 					<div class="row text-center">
 						<div class="col-lg-12">
 							<ul class="pagination">
-								<li>
-									<a href="#">&laquo;</a>
-								</li>
+								<li><a href="#">&laquo;</a></li>
 								<li class="active"><a href="#">1</a></li>
 								<li><a href="#">2</a></li>
 								<li><a href="#">3</a></li>
